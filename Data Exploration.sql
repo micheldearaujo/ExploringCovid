cecffd67bd9b9cc8a1f98750ebf5f45371588886
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2021-07-20 19:11:41 -0300

    Half of the way of the first part of the exploratory analysis

---
+++ Data Exploration.sql
@@ -0,0 +1,177 @@
+/*---------------------------------------------------------------------------------------------------------
+       
+                            Covid Data Exploration Projet
+In this project I will perform a exploratory data analysis and data cleaning a a dataset from Covid-19
+In this first part of the project I will walk through the dataset and try to make some discovery
+
+
+*/ --------------------------------------------------------------------------------------------------------
+-- From the original downloaded dataset we have extracted two tables, one containing the new cases and deaths info
+-- And other containing the tests and vacciations information:
+
+SELECT * FROM death;
+SELECT * FROM vaccine;
+
+-- Lets take a look at our tables
+
+SELECT *    -- Ordering the death data by the location and date
+FROM death
+ORDER BY 3, 4;
+
+SELECT *    -- Ordering the vaccination data by the location and date
+FROM vaccine
+ORDER BY 3, 4;
+
+
+--------------\\ Lets filter our data // -------------------------
+
+SELECT LOCATION, date, new_cases, total_cases, total_deaths, population
+FROM death
+WHERE continent IS NOT NULL;
+ORDER BY "location", "date";
+
+-- At this point we can already answer some questions, like
+-- What is the percentege of death for each contry through time? In other words,
+-- What is the probability of died if you get Covid?
+-- We answer this question by create a new column that is a division of the total_deaths by total_cases
+
+SELECT "location", "date", new_cases, total_cases, total_deaths, population,
+    concat((total_deaths::FLOAT/total_cases::FLOAT)*100, '%') AS "lethality_rate"
+FROM death
+WHERE continent IS NOT NULL;
+ORDER BY "location", "date";
+
+
+-- Now lets take a closer look at Brazil, the country I live in.
+SELECT "location", "date", new_cases, total_cases, total_deaths, population,
+    concat(round((total_deaths::NUMERIC/total_cases::NUMERIC)*100, 2), '%') AS "lethality_rate"
+FROM death
+WHERE "location" = 'Brazil' AND continent IS NOT NULL;
+ORDER BY "location", "date";
+
+
+-- What is the percentage of people that has got Covid?
+SELECT "location", "date", new_cases, total_cases, total_deaths, population,
+    concat(round((total_cases::NUMERIC/population::NUMERIC)*100, 2), '%') AS "infection_rate"
+FROM death
+WHERE "location" = 'Brazil'
+ORDER BY "location", "date";
+
+
+-- Now lets see the countries with the higher overall infection rate
+-- We need to select the highest infection_rate of each country
+
+SELECT "location", max(total_cases::NUMERIC) AS "total_cases",
+    round((max(total_cases::NUMERIC/population::NUMERIC))*100, 2) AS "infection_rate"
+FROM death
+WHERE continent IS NOT NULL
+GROUP BY "location"
+ORDER BY "infection_rate" DESC;
+
+
+-- What are the countries with the highest death rate?
+
+SELECT "location", max(total_deaths::NUMERIC) AS "total_deaths",
+    round((max(total_deaths::NUMERIC/total_cases::NUMERIC))*100, 2) AS "lethality_rate"
+FROM death
+WHERE ("total_deaths" IS NOT NULL) AND (continent IS NOT NULL)
+GROUP BY "location"
+ORDER BY "lethality_rate" DESC;
+-- There were some countries with 100% of death rate! That is really sad.
+
+-- If you can see, there is a issue with our data. There are locations called "South America", "Asia", and others,
+-- when it was supposed to be a country name, not a continent.
+
+SELECT * FROM death
+WHERE continent IS NOT NULL; -- Now we have got to add this condition in all the above queries
+
+
+--
+
+----- \\ Let's break things down by continent now // --------
+
+-- Showing the continents with highest death count
+SELECT "location", max(total_deaths::NUMERIC) AS "max_deaths"
+FROM death
+WHERE continent IS NULL
+GROUP BY "location"
+ORDER BY "max_deaths" DESC;
+
+SELECT "continent", max(total_deaths::NUMERIC) AS "max_deaths"
+FROM death
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY "max_deaths" DESC;
+
+
+
+
+------ \\ Formating the data to vizualize it in Tableau later on // ------
+
+-- Global numbers
+-- Total new cases, new deaths and death rates across the world
+
+SELECT "date", sum(new_cases::NUMERIC) AS "global_new_cases",
+                sum(new_deaths::NUMERIC) AS "total_new_deaths",
+                round(sum(new_deaths::NUMERIC)/sum(new_cases::NUMERIC), 2)*100 AS "global_lethallity_rate"
+FROM death
+WHERE continent IS NOT NULL
+GROUP BY "date"
+ORDER BY "date";
+
+
+-- And, the overall sum:
+
+SELECT sum(new_cases::NUMERIC) AS "global_new_cases",
+                sum(new_deaths::NUMERIC) AS "total_new_deaths",
+                round(sum(new_deaths::NUMERIC)/sum(new_cases::NUMERIC), 2)*100 AS "global_lethallity_rate"
+FROM death
+WHERE continent IS NOT NULL
+--group by "date"
+--order by "date";
+
+
+------ \\ Now lets combine our two tables in one // --------
+
+SELECT * FROM death AS d
+JOIN vaccine AS v USING("location", "date")
+ORDER BY LOCATION, date;
+
+
+-- Lets look at the percentage of people in the world that are vaccinated
+-- For this we can create a cumulative sum of the passing days
+
+-- the basic select
+SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
+FROM death AS d
+JOIN vaccine AS v USING("location", "date")
+ORDER BY LOCATION, date;
+
+-- Now lets create the cumulative sum
+SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations,
+        sum(v.new_vaccinations::NUMERIC)
+            OVER (PARTITION BY d.location
+                    ORDER BY d.location, d.date) AS "cumulative_vaccination"
+            
+FROM death AS d
+JOIN vaccine AS v USING("location", "date")
+ORDER BY LOCATION, date;
+
+-- Now we now how many people were vaccinated each day and the total vaccinated people at a specific date.
+-- Now we can create another column with the percentage of the population vaccinated each day
+
+SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations,
+        sum(v.new_vaccinations::NUMERIC)
+            OVER (PARTITION BY d.location
+                    ORDER BY d.location, d.date) AS "cumulative_vaccination",
+                    
+                    -- the method bellow is quite ugly to write although it works. But lets re-write this in a
+                    -- reader-friendly way
+        (sum(v.new_vaccinations::NUMERIC)
+            OVER (PARTITION BY d.location
+                    ORDER BY d.location, d.date)/d.population::NUMERIC)*100 AS "percentage_vaccinated"
+            
+FROM death AS d
+JOIN vaccine AS v USING("location", "date")
+ORDER BY LOCATION, date;
+
---
+++ column_filter.py
@@ -0,0 +1,39 @@
+# Separating some columns to make different tables
+
+import pandas as pd
+
+covid_data = pd.read_csv('owid-covid-data.csv')
+
+print(covid_data)
+
+print(covid_data.columns)
+
+# Creating a table only for the cases and deaths
+deaths = covid_data[['iso_code', 'continent', 'location', 'date', 'population','total_cases', 'new_cases',
+       'new_cases_smoothed', 'total_deaths', 'new_deaths',
+       'new_deaths_smoothed', 'total_cases_per_million',
+       'new_cases_per_million', 'new_cases_smoothed_per_million',
+       'total_deaths_per_million', 'new_deaths_per_million',
+       'new_deaths_smoothed_per_million', 'reproduction_rate', 'icu_patients',
+       'icu_patients_per_million', 'hosp_patients',
+       'hosp_patients_per_million', 'weekly_icu_admissions',
+       'weekly_icu_admissions_per_million', 'weekly_hosp_admissions',
+       'weekly_hosp_admissions_per_million']]
+
+# Creating another table only for the tests and vaccination and other information about the pacient
+vaccines = covid_data[['iso_code', 'continent', 'location', 'date', 'population', 'new_tests', 'total_tests',
+       'total_tests_per_thousand', 'new_tests_per_thousand',
+       'new_tests_smoothed', 'new_tests_smoothed_per_thousand',
+       'positive_rate', 'tests_per_case', 'tests_units', 'total_vaccinations',
+       'people_vaccinated', 'people_fully_vaccinated', 'new_vaccinations',
+       'new_vaccinations_smoothed', 'total_vaccinations_per_hundred',
+       'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
+       'new_vaccinations_smoothed_per_million', 'stringency_index', 
+       'population_density', 'median_age', 'aged_65_older',
+       'aged_70_older', 'gdp_per_capita', 'extreme_poverty',
+       'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers',
+       'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand',
+       'life_expectancy', 'human_development_index', 'excess_mortality']]
+
+#deaths.to_csv('./data/deaths.csv', index=False)
+vaccines.to_csv('./data/vaccination.csv', index=False)
